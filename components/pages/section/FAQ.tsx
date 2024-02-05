932f878e9cbeaeb79ec530ef9f490eb2b5080226
pncuong03 <phamngoccuonggl3022002@gmail.com>
2024-02-05 12:58:57 +0700

    fix faq

--- components/pages/section/FAQ.tsx
+++ components/pages/section/FAQ.tsx
@@ -6,14 +6,14 @@ const faqData = [
     key: "1",
     content:
       "XFarm.shop is an advanced service dedicated to enhancing visibility and interaction on Twitter. Our mission is to provide users with a unique and powerful Twitter experience, empowering them to build a robust online presence and achieve their personal or business goals in the realm of social media. ",
-    // img: "/images/What is XFarm.shop.png",
+    img: "/images/What is XFarm.shop.png",
   },
   {
     Header: "How to purchase a product?",
     key: "2",
     content:
       " To pay for your orders, top up your account first. The top-up and delivery process is automated, but it may take a few minutes for the system to process your request. ",
-    // img: "/images/How to purchase a product.png",
+    img: "/images/How to purchase a product.png",
   },
   {
     Header: "How to recharge?",
@@ -41,7 +41,7 @@ const faqData = [
 
     //   The money will automatically be credited to your account. You can top up any amount, starting from 1 VND
     // `,
-    // img: "/images/How to recharge.png",
+    img: "/images/How to recharge.png",
   },
   {
     Header:
@@ -49,21 +49,21 @@ const faqData = [
     key: "4",
     content:
       "We have seen this happen many times, so please be careful when depositing money. If you accidentally transfer money to the wrong account, please contact our support team immediately via tele/zalo. We will investigate the matter and return the money to the correct recipient. Please note that we will only return the money to the correct recipient to prevent scam cases.",
-    // img: "/images/What_is_the_process_to_recover_the_money_if_I_transferred_it_wrongly.png",
+    img: "/images/What_is_the_process_to_recover_the_money_if_I_transferred_it_wrongly.png",
   },
   {
     Header: "I made a deposit but the money didn't show up in my account. ",
     key: "5",
     content:
       "Occasionally, due to system errors, bank errors, or user errors such as entering the wrong syntax when depositing money, your balance may not be updated. Please do not worry and contact our support team immediately to have it updated.",
-    // img: "/images/made.png",
+      img: "/images/I made a deposit but the money didn't show up in my account.png",
   },
   {
     Header: " Is it possible to cancel my order? ",
     key: "6",
     content:
       "We do not offer order cancellation once the order has begun processing.Please note that we will not refund orders that are installed with incorrect information (incomplete information, wrong link, not public...) or that are faulty or die during the system's increase process. ",
-    // img: "/images/Is it possible to cancel my order.png",
+    img: "/images/Is it possible to cancel my order.png",
   },
   {
     Header: "What is the warranty policy?",
@@ -102,7 +102,7 @@ const faqData = [
         </ul>
       </>
     ),
-    // img: "/images/What is the warranty policy.png",
+    img: "/images/What is the warranty policy.png",
   },
 ];
 
@@ -124,7 +124,7 @@ const FAQ: React.FC = () => {
         {expandedItem === item.key && (
           <div className="faq-content">
             {item.content && <p>{item.content}</p>}
-            {/* {item.img && <img src={item.img} alt={item.Header} />} */}
+            {item.img && <img src={item.img} alt={item.Header} />}
           </div>
         )}
       </div>
